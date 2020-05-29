075031369c314b425b20c9c1c22e5be6f48e7fec
barabulka-ops <63974215+barabulka-ops@users.noreply.github.com>
2020-05-29 15:36:28 +0300

    Create nilActivity.java

---
+++ src/main/java/com/lingerman/nilActivity.java
@@ -0,0 +1,34 @@
+package com.lingerman;
+
+//////////////////////////////////пустое окно, чтобы перекрыть превью камеры
+
+import android.app.*;
+import android.os.*;
+
+import java.util.*; 
+import android.util.Log;
+
+import android.content.*;//  .Intent;Context;
+
+import android.view.*; 
+
+public class nilActivity extends Activity {
+
+      @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nil);
+
+//          Intent intent = new Intent();
+ 
+ 
+//          setResult(RESULT_OK, intent);
+//          finish();     
+ 
+      }
+
+
+
+}
